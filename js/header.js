f30a67a02a442ae874ce625732948afa15e7287f
P1ntas <72892065+P1ntas@users.noreply.github.com>
2024-11-19 09:29:48 -0300

    Fix 1

--- js/header.js
+++ js/header.js
@@ -3,18 +3,16 @@ const navMenu = document.querySelector('.nav-menu');
 
 menuToggle.addEventListener('click', function() {
     if (navMenu.classList.contains('active')) {
-        // Start closing transition by removing active class
+
         navMenu.classList.remove('active');
         
-        // Wait for transition to complete before hiding the menu
+
         setTimeout(() => {
             navMenu.style.display = 'none';
-        }, 300); // Match with CSS transition duration
+        }, 300); 
     } else {
-        // Show the menu with display: flex, then trigger the opening transition
         navMenu.style.display = 'flex';
         
-        // Allow a slight delay before adding 'active' to trigger the transition
         setTimeout(() => {
             navMenu.classList.add('active');
         }, 10);
